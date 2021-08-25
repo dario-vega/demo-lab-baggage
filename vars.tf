730735c4d67be3be3acca23345155635ca585a78
dario-vega <66302636+dario-vega@users.noreply.github.com>
2021-08-25 09:10:43 +0200

    Add files via upload

--- vars.tf
+++ vars.tf
@@ -1,2 +1,24 @@
-variable compartment_ocid {  }
 variable region {  }
+variable compartment_ocid {  }
+variable "application_id" {
+  default = ""
+}
+
+variable "ocir_repo_name" {
+  default = "nosql_demos"
+}
+
+variable "ocir_user_name" {
+  default = "ocid1.fnapp.oc1.eu-frankfurt-1.aaaaaaaaywrjowh6eevboplpkysl6efwjftb22xaj4gycx3km35tkizj7arq"
+}
+
+variable "ocir_user_password" {
+  default = ""
+}
+
+# OCIR repo name & namespace
+
+locals {
+  ocir_docker_repository = join("", [lower(lookup(data.oci_identity_regions.oci_regions.regions[0], "key" )), ".ocir.io"])
+  ocir_namespace = lookup(data.oci_identity_tenancy.oci_tenancy, "name" )
+}
