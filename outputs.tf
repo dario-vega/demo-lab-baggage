da215ecfbfa1e8e97156ef87ae0aa47c3e8da08f
dario-vega <66302636+dario-vega@users.noreply.github.com>
2021-08-25 18:01:29 +0200

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-## Copyright (c) 2020, Oracle and/or its affiliates.
-## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
-output "oci_apigateway_deployment_URL" {
-  value = [join("", [data.oci_apigateway_deployment.apigateway_deployment.endpoint, "/upload2stream"])]
-}
