ee0a482a1bd4c28e4966a43d34b7de6fc667fa41
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-08-30 15:33:46 +0200

    Create policies.tf

---
+++ policies.tf
@@ -0,0 +1,16 @@
+## Copyright (c) 2020, Oracle and/or its affiliates.
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+# Functions Policies
+
+resource "oci_identity_policy" "FunctionsServiceReposAccessPolicy" {
+  provider = oci.homeregion
+  name = "FunctionsServiceReposAccessPolicy"
+  description = "FunctionsServiceReposAccessPolicy"
+  compartment_id = var.tenancy_ocid
+  statements = ["Allow service FaaS to read repos in tenancy"]
+
+  provisioner "local-exec" {
+       command = "sleep 5"
+  }
+}
