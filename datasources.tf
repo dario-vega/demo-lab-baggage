ac5db14d7e09c960140d79eada545214c4e7d343
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-08-30 16:10:23 +0200

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -10,6 +10,15 @@ data "oci_identity_regions" "oci_regions" {
 
 }
 
+data "oci_identity_region_subscriptions" "home_region_subscriptions" {
+    tenancy_id = var.tenancy_ocid
+
+    filter {
+      name   = "is_home_region"
+      values = [true]
+    }
+}
+
 data "oci_identity_tenancy" "oci_tenancy" {
     tenancy_id = var.tenancy_ocid
 }
