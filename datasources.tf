0367d3171ba5ab262e084125fc5499cdec76ad8d
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-08-31 22:32:07 +0200

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -10,6 +10,14 @@ data "oci_identity_regions" "oci_regions" {
 
 }
 
+data "oci_core_services" "oci_services" {
+  filter {
+    name   = "name"
+    values = ["All .* Services In Oracle Services Network"]
+    regex  = true
+  }
+}
+
 data "oci_identity_region_subscriptions" "home_region_subscriptions" {
     tenancy_id = var.tenancy_ocid
 
