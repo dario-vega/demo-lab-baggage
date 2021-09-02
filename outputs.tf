2ac467e0a7fa4e8eacc8943343c2d22e1a330bd1
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-09-02 14:54:03 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,12 @@
 output "oci_apigateway_deployment_URL" {
   value = [join("", [data.oci_apigateway_deployment.export_BaggageDemo.endpoint, "/getBagInfoByTicketNumber"])]
 }
+
+output "nosql_table_ddl_statement" {
+  value = data.oci_nosql_table.nosql_demo.ddl_statement
+}
+
+output "nosql_kv_table_ddl_statement" {
+  value = data.oci_nosql_table.nosql_demoKeyVal.ddl_statement
+}
+
